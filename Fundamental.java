36f16977dd68052d4918e7eaea2ef42243098d5d
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-02-18 08:30:55 -0800

    second_commit

--- Fundamental.java
+++ Fundamental.java
@@ -1,148 +1,168 @@
-//オブジェクト指向のgetter/setterの使い方
+// //オブジェクト指向のgetter/setterの使い方
 
-//getter/setterメソッドの使用例
-class User{
-    private String name;
+// //getter/setterメソッドの使用例
+// class User{
+//     private String name;
 
-    public String getName(){
-        return name;
-    }
+//     public String getName(){
+//         return name;
+//     }
 
-    public void setName(String name){
-        this.name = name;
-    }
-}
+//     public void setName(String name){
+//         this.name = name;
+//     }
+// }
 
-//getter/setterメソッドはコードが冗長になってしまうという欠点も存在
-class User{
-    private String name;
-    private int age;
-    private boolean isAdmin;
+// //getter/setterメソッドはコードが冗長になってしまうという欠点も存在
+// class User{
+//     private String name;
+//     private int age;
+//     private boolean isAdmin;
 
-    public String getName(){
-        return name;
-    }
+//     public String getName(){
+//         return name;
+//     }
 
-    public void setName(String name){
-        this.name = name;
-    }
+//     public void setName(String name){
+//         this.name = name;
+//     }
 
-    public int getAge(){
-        return age;
-    }
+//     public int getAge(){
+//         return age;
+//     }
 
-    public void setAge(int age){
-        this.age = age;
-    }
+//     public void setAge(int age){
+//         this.age = age;
+//     }
 
-    public boolean getAdmin(){
-        return isAdmin;
-    }
+//     public boolean getAdmin(){
+//         return isAdmin;
+//     }
 
-    public void setAdmin(boolean isAdmin){
-        this.isAdmin = isAdmin;
-    }
-}
+//     public void setAdmin(boolean isAdmin){
+//         this.isAdmin = isAdmin;
+//     }
+// }
 
-//PI(Persistence Ignorance)は、「ただの構造体として振る舞う」クラスのパターン。POJO(Plain old java object)とも呼ばれる。
-//このパターンを用いると、上記のJavaコードは次のように書き換えられる。
-class User{
-    public String name;
-    public int age;
-    public boolean isAdmin;
-}
+// //PI(Persistence Ignorance)は、「ただの構造体として振る舞う」クラスのパターン。POJO(Plain old java object)とも呼ばれる。
+// //このパターンを用いると、上記のJavaコードは次のように書き換えられる。
+// class User{
+//     public String name;
+//     public int age;
+//     public boolean isAdmin;
+// }
 
-//Ruby on RailsやLaravel等のActiveRecordでは、コード上では定義されていないフィールドに動的にアクセスすることができる。
-class User extends Model{
+// //Ruby on RailsやLaravel等のActiveRecordでは、コード上では定義されていないフィールドに動的にアクセスすることができる。
+// class User extends Model{
 
-}
+// }
 
-$user = User::first();
-$user->id;
-$user->name;
+// $user = User::first();
+// $user->id;
+// $user->name;
 
-//といったようにアクセスでき、当然こういった場合はgetter/setterメソッドをいちいち定義する必要はない。
-
-
-//java try-catch文
-//基本構造
-
-//tryブロックでは例外が発生しうる処理を書きます。もしtryブロック内で例外が発生したなら、Javaがその例外を捕まえて、catchブロックに処理を移します。catchブロック内には補足した例外への処理を書いておきます。
-try{
-    //例外が発生しうる処理
-}catch(/* 捕まえる例外の型　例外の変数 */){
-    //例外発生時の処理
-}
-
-public FileReader(String fileName) throw FileNotFoundEXCEPTION
-
-try{
-    FileReader fr = new FileReader("/A/B/C/D.txt") //ここで発生しうるFileNotFoundExceptionを...
-}catch(FileNotFoundEXCEPTION e){ /// ここでcatchして…
-    System.out.println("ファイルがありません！！"); //// 何かのフォロー処理を行う!!
-}
-//ここでのeはexceptionのe
+// //といったようにアクセスでき、当然こういった場合はgetter/setterメソッドをいちいち定義する必要はない。
 
 
-//例外が発生すると処理は中断される
-//以下のプログラムでは、処理の実行順序は①→②→④→⑤となり、③は実行されません。例外が途中で発生しなければ①→②→③→⑤となり、catchブロックの内容(④)は実行されない
-System.out.println("①");
+// //java try-catch文
+// //基本構造
 
-try{
-    System.out.println("②");
-    if (true){// ※if文はコンパイルエラーを回避するためで、実際には不要
-        throw new Exception(); // 例外が発生!!
-    }
-    System.out.println("③");
-}catch(Exception e){
-    System.out.println("④");
-}
-System.out.println("⑤");
+// //tryブロックでは例外が発生しうる処理を書きます。もしtryブロック内で例外が発生したなら、Javaがその例外を捕まえて、catchブロックに処理を移します。catchブロック内には補足した例外への処理を書いておきます。
+// try{
+//     //例外が発生しうる処理
+// }catch(/* 捕まえる例外の型　例外の変数 */){
+//     //例外発生時の処理
+// }
+
+// public FileReader(String fileName) throw FileNotFoundEXCEPTION
+
+// try{
+//     FileReader fr = new FileReader("/A/B/C/D.txt") //ここで発生しうるFileNotFoundExceptionを...
+// }catch(FileNotFoundEXCEPTION e){ /// ここでcatchして…
+//     System.out.println("ファイルがありません！！"); //// 何かのフォロー処理を行う!!
+// }
+// //ここでのeはexceptionのe
 
 
-//finallyで必ず実行する処理を記述する方法
-//ここではfinallyの使い方を解説します。finallyブロックの中の処理は、例外の有無に関わらず、最後に必ず実行されます。そのため、finallyはファイルを開いた後に確実にcloseメソッドで閉じたいときなどに使用される。
+// //例外が発生すると処理は中断される
+// //以下のプログラムでは、処理の実行順序は①→②→④→⑤となり、③は実行されません。例外が途中で発生しなければ①→②→③→⑤となり、catchブロックの内容(④)は実行されない
+// System.out.println("①");
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException; //
+// try{
+//     System.out.println("②");
+//     if (true){// ※if文はコンパイルエラーを回避するためで、実際には不要
+//         throw new Exception(); // 例外が発生!!
+//     }
+//     System.out.println("③");
+// }catch(Exception e){
+//     System.out.println("④");
+// }
+// System.out.println("⑤");
 
-public class Main{
 
-    public static void main(String[] args){
-        //1.ファイルのパスを指定する.
-        File file = new File("c:\\Sample\\Test.txt");
+// //finallyで必ず実行する処理を記述する方法
+// //ここではfinallyの使い方を解説します。finallyブロックの中の処理は、例外の有無に関わらず、最後に必ず実行されます。そのため、finallyはファイルを開いた後に確実にcloseメソッドで閉じたいときなどに使用される。
 
-        //2.ファイルが存在しない時び例外が発生するので確認する.
-        if(!file.exist()){
-            System.out.println("ファイルがぞんざいしません");
-            return;
-        }
+// import java.io.File;
+// import java.io.FileReader;
+// import java.io.IOException; //
 
-        //3.FileReaderクラスとreadメソッドを使って1文字ずつ読み込み表示する
-        FileReader fileReader = null;
-        try{
-            fileReader = new FileReader(file);
+// public class Main{
 
-            int data;
-            while((data = fileReader.read() != -1)){
-                System.out.print((char) data);
-            }
+//     public static void main(String[] args){
+//         //1.ファイルのパスを指定する.
+//         File file = new File("c:\\Sample\\Test.txt");
 
-        }catch(IOException e){
-            e.printStackTrace();
-        }finally{
-            //4.最後にファイルを閉じてリソースを確認する
-        }try{
-            if(fileReader != null){
-                fileReader.close();
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-}
+//         //2.ファイルが存在しない時び例外が発生するので確認する.
+//         if(!file.exist()){
+//             System.out.println("ファイルがぞんざいしません");
+//             return;
+//         }
+
+//         //3.FileReaderクラスとreadメソッドを使って1文字ずつ読み込み表示する
+//         FileReader fileReader = null;
+//         try{
+//             fileReader = new FileReader(file);
+
+//             int data;
+//             while((data = fileReader.read() != -1)){
+//                 System.out.print((char) data);
+//             }
+
+//         }catch(IOException e){
+//             e.printStackTrace();
+//         }finally{
+//             //4.最後にファイルを閉じてリソースを確認する
+//         }try{
+//             if(fileReader != null){
+//                 fileReader.close();
+//             }
+//         }catch(IOException e){
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
 //表示結果　SAMURAI　ENGINEER
 
 
+//java継承
+// 親クラス←小クラス
+
+//EXIT_ON_CLOSE (WindowConstantsで定義されています): System exitメソッドを使用してアプリケーションを終了します。 アプリケーションでのみ使用する。
+
+//setBounds:フレームのサイズを設定する
+
+import javax.swing.JFrame;
+
+class Fundamental{
+    public static void main(String[] args){
+        JFrame frame1 = new JFrame("My Title1");
+        frame1.setBounds(100, 100, 600, 400);
+        frame1.setVisible(true);
+
+        JFrame frame2 = new JFrame("My Title2");
+        frame2.setBounds(100, 100, 600, 400);
+        frame2.setVisible(true);
+    }
+}
